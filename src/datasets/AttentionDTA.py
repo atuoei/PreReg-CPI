dd40ecf0dc1c75b56ed6a8b87e28afa17bf613f3
atuoei <157084464+atuoei@users.noreply.github.com>
2025-11-22 14:17:49 +0800

    Update AttentionDTA.py

--- src/datasets/AttentionDTA.py
+++ src/datasets/AttentionDTA.py
@@ -79,7 +79,7 @@ def _collate(batch,max_d=150,max_p=1000):
     return gene,compound_new, protein_new, label
 
 class Modeldataset:
-    def __init__(self, df_train, df_val, df_test, pro_dir='../Features/targets.csv', batch_size=128, num_workers=4):
+    def __init__(self, df_train, df_val, df_test, pro_dir='Features/targets.csv', batch_size=128, num_workers=4):
         self.train_ds = _Dataset(df_train, pro_dir)
         self.val_ds   = _Dataset(df_val,   pro_dir)
         self.test_ds  = _Dataset(df_test,  pro_dir)
@@ -92,4 +92,5 @@ class Modeldataset:
                            num_workers=0, collate_fn=_collate)
         test  = DataLoader(self.test_ds, batch_size=self.bs, shuffle=False,
                            num_workers=0, collate_fn=_collate)
+
         return train, val, test
